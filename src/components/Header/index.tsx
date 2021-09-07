123edd778e5a93d04ce0000b3de990767b53ff3c
mihailgaberov <mihail.gaberov@gmail.com>
2021-09-07 18:46:27 +0200

    :sparkles: Added Header component with title, select inputs and styles for responsiveness.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,18 @@
 import React, { FunctionComponent } from 'react';
-import { Container } from "./styles";
+import { Container, SelectsContainer } from "./styles";
 
 const Header: FunctionComponent = () => {
   return (
     <Container>
       <h1>EUROJACKPOT RESULTS & WINNING NUMBERS</h1>
-      <div>
+      <SelectsContainer>
         <select name="date">
           <option>Fri 03 Sep</option>
         </select>
         <select name="year">
           <option>2021</option>
         </select>
-      </div>
+      </SelectsContainer>
     </Container>
   );
 };
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -12,9 +12,30 @@ export const Container = styled.div`
   
   h1 {
     color: #012406;
+    font-weight: 900;
+    font-size: 1.1em;
+    line-height: 1.2em;
+    -webkit-font-smoothing: antialiased;
+    margin: 1em 0;
   }
 
-  @media only screen and (min-width: 800px) {
+  @media only screen and (min-width: 920px) {
     flex-direction: row;
+    font-size: 1.2em;
+    line-height: 1.3em;
+  }
+`
+
+export const SelectsContainer = styled.div`
+  display: flex;
+  width: 12.5em;
+  justify-content: space-between;
+  
+  select {
+    font-size: 1.2em;
+    padding: .3em;
+    border-radius: 3px;
+    border: 1px solid #d9deda;
+    color: #012406;
   }
 `
