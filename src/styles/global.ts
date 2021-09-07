b469dcd1e6bd5a128ea0545c387a3a2c41f7c82d
mihailgaberov <mihail.gaberov@gmail.com>
2021-09-07 17:42:00 +0200

    :sparkles: Installed styled-components. Added global styles.

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: FF Daxline Pro, Geneva, Trebuchet MS, Tahoma, Verdana, sans-serif;
+  }
+
+  #root {
+    margin: 0 auto;
+  }
+
+  body {
+    background-color: white;
+  }
+`
