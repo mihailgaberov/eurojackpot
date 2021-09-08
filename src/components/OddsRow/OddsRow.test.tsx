d23a32839f58b647188828f79a03e4faa2e37946
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-09-08 18:00:02 +0200

    :white_check_mark: Added tests for OddsRow component.

---
+++ src/components/OddsRow/OddsRow.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import OddsRow from './index';
+import { mapMatchesToIndexes } from "../../helpers";
+
+
+test('renders primary MessageCard component correctly', () => {
+  render(<OddsRow order={1} amount={'33000'} match={mapMatchesToIndexes.get('1')} isTitle={true} tier='I' winners={'22'} /> );
+  const tierElement = screen.getByText(/Tier/i);
+  expect(tierElement).toBeInTheDocument();
+  const winnersElement = screen.getByText(/Winners/i);
+  expect(winnersElement).toBeInTheDocument();
+  const matchElement = screen.getByText(/Match/i);
+  expect(matchElement).toBeInTheDocument();
+});
